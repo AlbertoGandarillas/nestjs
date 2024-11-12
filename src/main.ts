6926ad9134399bcea4e8cc820afe81939b0f36c2
AlbertoGandarillas <albertogandarillas@hotmail.com>
2024-11-12 15:26:32 -0500

    Update settings

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-const app = await NestFactory.create(AppModule);
-app.setGlobalPrefix('api');
-app.enableCors();
-await app.listen(process.env.PORT ?? 4000);
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('CPL API')
+      .setDescription(
+        'API documentation for CPL College Landing pages and CPL Dashboard projects',
+      )
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 4000);
 }
+export default bootstrap;
+
 bootstrap();
